9fa2baefcb145eae3eade32ea4d13f15f120e0b7
JeremieBrun88 <entreprise.brunj@gmail.com>
2025-05-20 15:29:07 +0200

    15h30

--- app.js
+++ app.js
@@ -2,39 +2,70 @@ fetch("data.json")
     .then(response => response.json())
     .then(data => {
 
+        const imput = document.getElementById("recherche")
+        let trie = []
+        imput.addEventListener("input", () => {
+            document.getElementById("container").innerHTML = ""
+            console.log(imput.value)
+            trie = data.filter(recette => {
+                return recette.nom.includes(imput.value)
+            });
+            if (trie.length > 0) {
+                afficher(trie)
+            } else {
+                afficher(data)
+            }
 
+        })
+
+        // de toute les manières, j'affiche data //
         afficher(data)
 
     });
 
 
+
+
+
+// // Pour chacun des ingrédients, de récupère le tableau (ingrédients) et je le "forEach" dans la liste //
+
+function afficheringredients(tableauingredients) {
+    let liste = ""
+    tableauingredients.forEach(i => {
+        liste += `<li>${i.quantite} ${i.unite} ${i.aliment}</li>`
+    });
+
+    return liste
+}
+
+// Pour chacune des étapes, de récupère le tableau (étapes) et je le "forEach" dans la liste //
+
+function afficherEtape(tableauEtape) {
+    let liste = ""
+    tableauEtape.forEach(etapes => {
+        liste += `<li> ${etapes.descEtape}</li>`
+    });
+    return liste
+}
+
+// je veux afficher les informations au bon endroit, je let les "sous tableaux" //
+
 function afficher(donnee) {
 
     donnee.forEach(recette => {
-        console.log(recette.nom)
-        console.log(recette.img)
-        console.log(recette.desc)
-        console.log(recette.difficulte)
-        console.log(recette.portions)
-        console.log(recette.tempPreparation)
-        console.log(recette.tempCuisson)
-   
-
-        // pour chacun des ingredients, je conduit ingredients.aliment //
 
 
-    recette.ingredients.forEach(i => {
-        console.log(i.quantite)
-        recette.ingredients.forEach(i => {
-            console.log(i.unite)
-        })
-    })    
- 
-
-        document.body.innerHTML += `
+        let ingredientsLi = afficheringredients(recette.ingredients)
+        let etapes = afficherEtape(recette.etapes)
 
 
-    <section class="container">
+
+        // je récupère les éléments du Html //
+
+
+        document.getElementById("container").innerHTML += `
+
+
         <div class="card">
             <div class="flex gap16 align-center">
                 <h1>${recette.nom}</h1>
@@ -51,17 +82,17 @@ function afficher(donnee) {
                 <div class="large-3">
                     <div class="align-start flex gap16">
                         <div class="large-12">
-                            <img src="./assets/curry1.jpg" alt="" class="responsive">
+                            <img src="${recette.img}" alt="" class="responsive">
                             <div class="legende">${recette.nom}</div>
                         </div>
                         <div class="large-4">
-                            <img src="assets/curry2.jpg" alt="" class="responsive">
+                            <img  src="${recette.img[1]}" alt="" class="responsive">
                         </div>
                         <div class="large-4">
-                            <img src="/assets/curry3.jpg" alt="" class="responsive">
+                            <img src="${recette.img[2]}" alt="" class="responsive">
                         </div>
                         <div class="large-4">
-                            <img src="/assets/curry4.jpg" alt="" class="responsive">
+                            <img src="${recette.img[3]}" alt="" class="responsive">
                         </div>    
                     </div>
                 </div>
@@ -87,18 +118,7 @@ function afficher(donnee) {
                     <div>
                         <h2>Ingrédients</h2>
                         <ul>
-                            <li>${recette.ingredients}</li>
-                            <li>${recette.ingredients[1]}</li> 
-                            <li>${recette.ingredients[2]}</li>
-                            <li>${recette.ingredients[3]}</li>
-                            <li>${recette.ingredients[4]}</li>
-                            <li>${recette.ingredients[5]}</li>
-                            <li>${recette.ingredients[6]}</li>
-                            <li>${recette.ingredients[7]}</li>
-                            <li>${recette.ingredients[8]}</li>
-                            <li>${recette.ingredients[9]}</li>
-                            <li>${recette.ingredients[10]}</li>
-                            <li>${recette.ingredients[11]}</li>
+                           ${ingredientsLi} 
                         </ul>
                     </div>
 
@@ -107,25 +127,13 @@ function afficher(donnee) {
                 <div class="large-6">
                     <h2>Etapes</h2>
                     <ol>
-                        <li>Émincez l'oignon, hachez l’ail, râpez ou hachez le gingembre, et découpez le poulet en morceaux de taille égale.</li>
-                        <li>Dans une grande poêle ou une cocotte, faites chauffer un peu d’huile. Ajoutez l’oignon et faites-le revenir jusqu’à ce qu’il soit translucide. Ajoutez ensuite l’ail et le gingembre, puis faites revenir 1 à 2 minutes de plus</li>
-                        <li>Ajoutez les morceaux de poulet dans la poêle et faites-les dorer de tous les côtés</li>
-                        <li>Saupoudrez le curry en poudre sur le poulet et mélangez bien pour enrober tous les ingrédients. Laissez cuire 1 à 2 minutes pour faire ressortir les arômes</li>
-                        <li>
-                            Ajoutez les tomates en dés (avec leur jus) et le lait de coco. Mélangez bien. Portez à ébullition, puis réduisez le feu et laissez mijoter à couvert pendant environ 20 minutes, ou jusqu’à ce que le poulet soit bien cuit et la sauce légèrement épaissie.</li>
-                        <li>
-
-                            Salez et poivrez selon votre goût. Si vous aimez les plats plus épicés, vous pouvez ajouter des flocons de piment ou du piment frais à ce stade</li>
-                        <li>Parsemez de coriandre fraîche hachée et servez chaud avec du riz basmati ou du pain naan.</li>
+                        ${etapes}
                     </ol>
                 </div>
             </div>
         </div>
-    </section>
-
-` 
-
-    });
-
-
+`
+    })
 }
+
+
